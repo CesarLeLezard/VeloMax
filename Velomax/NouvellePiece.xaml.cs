38294e127f6cf13eade881d77e75184ebd05bb32
zdvbkd <alexandre.grosse92@gmail.com>
2021-05-14 02:10:46 +0200

    suppression des pièces fonctionne

--- Velomax/NouvellePiece.xaml.cs
+++ Velomax/NouvellePiece.xaml.cs
@@ -22,14 +22,17 @@ namespace Velomax
     {
         private MySqlConnection maConnexion;
 
+        private StockPieces parent;
+
         private SortedList<int, string> categories;
         private SortedList<string, string> fournisseurs;
 
-        public NouvellePiece(MySqlConnection maConnexion)
+        public NouvellePiece(MySqlConnection maConnexion, StockPieces parent)
         {
             InitializeComponent();
 
             this.maConnexion = maConnexion;
+            this.parent = parent;
             InitCategories();
             InitFournisseurs();
         }
@@ -38,54 +41,70 @@ namespace Velomax
         {
             categories = new SortedList<int, string>();
 
-            maConnexion.Open();
-
-            MySqlCommand command = maConnexion.CreateCommand();
-            command.CommandText = "SELECT id_categorie, lib_categorie FROM categorie ORDER BY lib_categorie;";
-
-            MySqlDataReader reader;
-            reader = command.ExecuteReader();
-
-            int idCategorie;
-            string libCategorie;
-
-            while (reader.Read())   // parcours ligne par ligne
+            try
             {
-                idCategorie = reader.GetInt32(0); // récupération 1ère colonne contenant les id_categorie
-                libCategorie = reader.GetString(1); // récupération 2ème colonne contenant les libellés des catégories
-                categories.Add(idCategorie, libCategorie);
-            }
+                maConnexion.Open();
 
-            maConnexion.Close();
-            
-            cbCategories.ItemsSource = categories.Values;
+                MySqlCommand command = maConnexion.CreateCommand();
+                command.CommandText = "SELECT id_categorie, lib_categorie FROM categorie ORDER BY lib_categorie;";
+
+                MySqlDataReader reader;
+                reader = command.ExecuteReader();
+
+                int idCategorie;
+                string libCategorie;
+
+                while (reader.Read())   // parcours ligne par ligne
+                {
+                    idCategorie = reader.GetInt32(0); // récupération 1ère colonne contenant les id_categorie
+                    libCategorie = reader.GetString(1); // récupération 2ème colonne contenant les libellés des catégories
+                    categories.Add(idCategorie, libCategorie);
+                }
+                cbCategories.ItemsSource = categories.Values;
+            }
+            catch (MySqlException erreur)
+            {
+                MessageBox.Show("Erreur de requête SQL :\n" + erreur);
+            }
+            finally
+            {
+                maConnexion.Close();
+            }
         }
 
         private void InitFournisseurs()
         {
             fournisseurs = new SortedList<string, string>();
 
-            maConnexion.Open();
-
-            MySqlCommand command = maConnexion.CreateCommand();
-            command.CommandText = "SELECT siret_fourn, nom_fourn FROM fournisseur ORDER BY nom_fourn;";
-
-            MySqlDataReader reader;
-            reader = command.ExecuteReader();
-
-            string siretFournisseur;
-            string nomFournisseur;
-
-            while (reader.Read())   // parcours ligne par ligne
+            try
             {
-                siretFournisseur = reader.GetString(0); // récupération 1ère colonne contenant les id_categorie
-                nomFournisseur = reader.GetString(1); // récupération 2ème colonne contenant les libellés des catégories
-                fournisseurs.Add(siretFournisseur, nomFournisseur);
+                maConnexion.Open();
+
+                MySqlCommand command = maConnexion.CreateCommand();
+                command.CommandText = "SELECT siret_fourn, nom_fourn FROM fournisseur ORDER BY nom_fourn;";
+
+                MySqlDataReader reader;
+                reader = command.ExecuteReader();
+
+                string siretFournisseur;
+                string nomFournisseur;
+
+                while (reader.Read())   // parcours ligne par ligne
+                {
+                    siretFournisseur = reader.GetString(0); // récupération 1ère colonne contenant les siret des fournisseurs
+                    nomFournisseur = reader.GetString(1); // récupération 2ème colonne contenant les noms des fournisseurs
+                    fournisseurs.Add(siretFournisseur, nomFournisseur);
+                }
+                cbFournisseurs.ItemsSource = fournisseurs.Values;
             }
-
-            maConnexion.Close();
-
-            cbFournisseurs.ItemsSource = fournisseurs.Values;
+            catch (MySqlException erreur)
+            {
+                MessageBox.Show("Erreur de requête SQL :\n" + erreur);
+            }
+            finally
+            {
+                maConnexion.Close();
+            }
         }
 
         private void bValider_Click(object sender, RoutedEventArgs e)
@@ -170,80 +189,81 @@ namespace Velomax
                                                 {
                                                     DateTime dateDisc = (DateTime)dpDateDisc.SelectedDate;
 
-
-                                                    maConnexion.Open();
-
-                                                    MySqlParameter id_piece = new MySqlParameter("@id_piece", MySqlDbType.VarChar);
-                                                    id_piece.Value = idPiece;
-
-                                                    MySqlParameter prix_piece = new MySqlParameter("@prix_piece", MySqlDbType.Float);
-                                                    prix_piece.Value = prix;
-
-                                                    MySqlParameter dateIntro_piece = new MySqlParameter("@dateIntro_piece", MySqlDbType.Date);
-                                                    dateIntro_piece.Value = dateIntro;
-
-                                                    MySqlParameter dateDisc_piece = new MySqlParameter("@dateDisc_piece", MySqlDbType.Date);
-                                                    dateDisc_piece.Value = dateDisc;
-
-                                                    MySqlParameter stock_piece = new MySqlParameter("@stock_piece", MySqlDbType.Int32);
-                                                    stock_piece.Value = quantite;
-
-                                                    MySqlParameter id_categorie = new MySqlParameter("@id_categorie", MySqlDbType.Int32);
-                                                    id_categorie.Value = categories.Keys[indexCategorie];
-
-                                                    MySqlParameter siret_fourn = new MySqlParameter("@siret_fourn", MySqlDbType.VarChar);
-                                                    siret_fourn.Value = fournisseurs.Keys[indexFournisseur];
-
-                                                    MySqlParameter prix_fournit = new MySqlParameter("@prix_fournit", MySqlDbType.Float);
-                                                    prix_fournit.Value = prixFournisseur;
-
-                                                    MySqlParameter delai_fournit = new MySqlParameter("@delai_fournit", MySqlDbType.Int32);
-                                                    delai_fournit.Value = delai;
-
-                                                    MySqlParameter numCatalogue_fournit = new MySqlParameter("@numCatalogue_fournit", MySqlDbType.VarChar);
-                                                    numCatalogue_fournit.Value = numCatalogue;
-
-                                                    MySqlParameter date_fournit = new MySqlParameter("@date_fournit", MySqlDbType.Date);
-                                                    date_fournit.Value = DateTime.Now.Date;
-
-                                                    MySqlParameter qte_fournit = new MySqlParameter("@qte_fournit", MySqlDbType.Int32);
-                                                    qte_fournit.Value = quantite;
-
-
-                                                    MySqlCommand command1 = maConnexion.CreateCommand();
-                                                    command1.CommandText = "INSERT INTO velomax.piece VALUES (@id_piece, @prix_piece, @dateIntro_piece, @dateDisc_piece, @stock_piece, @id_categorie);";
-                                                    command1.Parameters.Add(id_piece);
-                                                    command1.Parameters.Add(prix_piece);
-                                                    command1.Parameters.Add(dateIntro_piece);
-                                                    command1.Parameters.Add(dateDisc_piece);
-                                                    command1.Parameters.Add(stock_piece);
-                                                    command1.Parameters.Add(id_categorie);
-
-                                                    MySqlCommand command2 = maConnexion.CreateCommand();
-                                                    command2.CommandText = "INSERT INTO velomax.fournit VALUES (@id_piece, @siret_fourn, @prix_fournit, @delai_fournit, @numCatalogue_fournit, @date_fournit, @qte_fournit);";
-                                                    command2.Parameters.Add(id_piece);
-                                                    command2.Parameters.Add(siret_fourn);
-                                                    command2.Parameters.Add(prix_fournit);
-                                                    command2.Parameters.Add(delai_fournit);
-                                                    command2.Parameters.Add(numCatalogue_fournit);
-                                                    command2.Parameters.Add(date_fournit);
-                                                    command2.Parameters.Add(qte_fournit);
-
                                                     try
                                                     {
+                                                        maConnexion.Open();
+
+                                                        MySqlParameter id_piece = new MySqlParameter("@id_piece", MySqlDbType.VarChar);
+                                                        id_piece.Value = idPiece;
+
+                                                        MySqlParameter prix_piece = new MySqlParameter("@prix_piece", MySqlDbType.Float);
+                                                        prix_piece.Value = prix;
+
+                                                        MySqlParameter dateIntro_piece = new MySqlParameter("@dateIntro_piece", MySqlDbType.Date);
+                                                        dateIntro_piece.Value = dateIntro;
+
+                                                        MySqlParameter dateDisc_piece = new MySqlParameter("@dateDisc_piece", MySqlDbType.Date);
+                                                        dateDisc_piece.Value = dateDisc;
+
+                                                        MySqlParameter stock_piece = new MySqlParameter("@stock_piece", MySqlDbType.Int32);
+                                                        stock_piece.Value = quantite;
+
+                                                        MySqlParameter id_categorie = new MySqlParameter("@id_categorie", MySqlDbType.Int32);
+                                                        id_categorie.Value = categories.Keys[indexCategorie];
+
+                                                        MySqlParameter siret_fourn = new MySqlParameter("@siret_fourn", MySqlDbType.VarChar);
+                                                        siret_fourn.Value = fournisseurs.Keys[indexFournisseur];
+
+                                                        MySqlParameter prix_fournit = new MySqlParameter("@prix_fournit", MySqlDbType.Float);
+                                                        prix_fournit.Value = prixFournisseur;
+
+                                                        MySqlParameter delai_fournit = new MySqlParameter("@delai_fournit", MySqlDbType.Int32);
+                                                        delai_fournit.Value = delai;
+
+                                                        MySqlParameter numCatalogue_fournit = new MySqlParameter("@numCatalogue_fournit", MySqlDbType.VarChar);
+                                                        numCatalogue_fournit.Value = numCatalogue;
+
+                                                        MySqlParameter date_fournit = new MySqlParameter("@date_fournit", MySqlDbType.Date);
+                                                        date_fournit.Value = DateTime.Now.Date;
+
+                                                        MySqlParameter qte_fournit = new MySqlParameter("@qte_fournit", MySqlDbType.Int32);
+                                                        qte_fournit.Value = quantite;
+
+
+                                                        MySqlCommand command1 = maConnexion.CreateCommand();
+                                                        command1.CommandText = "INSERT INTO velomax.piece VALUES (@id_piece, @prix_piece, @dateIntro_piece, @dateDisc_piece, @stock_piece, @id_categorie);";
+                                                        command1.Parameters.Add(id_piece);
+                                                        command1.Parameters.Add(prix_piece);
+                                                        command1.Parameters.Add(dateIntro_piece);
+                                                        command1.Parameters.Add(dateDisc_piece);
+                                                        command1.Parameters.Add(stock_piece);
+                                                        command1.Parameters.Add(id_categorie);
+
+                                                        MySqlCommand command2 = maConnexion.CreateCommand();
+                                                        command2.CommandText = "INSERT INTO velomax.fournit VALUES (@id_piece, @siret_fourn, @prix_fournit, @delai_fournit, @numCatalogue_fournit, @date_fournit, @qte_fournit);";
+                                                        command2.Parameters.Add(id_piece);
+                                                        command2.Parameters.Add(siret_fourn);
+                                                        command2.Parameters.Add(prix_fournit);
+                                                        command2.Parameters.Add(delai_fournit);
+                                                        command2.Parameters.Add(numCatalogue_fournit);
+                                                        command2.Parameters.Add(date_fournit);
+                                                        command2.Parameters.Add(qte_fournit);
+
+
                                                         command1.ExecuteNonQuery();
                                                         command2.ExecuteNonQuery();
                                                         MessageBox.Show("Pièce ajoutée !");
+                                                        this.Close();
                                                     }
                                                     catch (MySqlException erreur)
                                                     {
                                                         MessageBox.Show("Erreur à l'ajout de la pièce :\n" + erreur);
                                                     }
-
-                                                    // categories.Keys[indexCategorie] -> id_categorie choisie
-                                                    // fournisseurs.Keys[indexFournisseur] -> siret_fourn choisie
-
-                                                    maConnexion.Close();
+                                                    finally
+                                                    {
+                                                        maConnexion.Close();
+                                                        this.parent.LoadInfosPieces(); // actualisation de la liste
+                                                    }
                                                 }
                                             }
                                         }
--- Velomax/StockPieces.xaml.cs
+++ Velomax/StockPieces.xaml.cs
@@ -32,24 +32,76 @@ namespace Velomax
         }
 
 
-        private void LoadInfosPieces()
+        public void LoadInfosPieces()
         {
-            maConnexion.Open();
+            try
+            {
+                maConnexion.Open();
 
-            MySqlCommand command = maConnexion.CreateCommand();
-            command.CommandText = "SELECT id_piece, lib_categorie, prix_piece, dateIntro_piece, dateDisc_piece, stock_piece FROM piece NATURAL JOIN categorie ORDER BY id_piece;";
+                MySqlCommand command = maConnexion.CreateCommand();
+                command.CommandText = "SELECT id_piece, lib_categorie, prix_piece, dateIntro_piece, dateDisc_piece, stock_piece FROM piece NATURAL JOIN categorie ORDER BY id_piece;";
 
-            DataTable dt = new DataTable();
-            dt.Load(command.ExecuteReader());
-            dgPieces.ItemsSource = dt.DefaultView;
-
-            maConnexion.Close();
+                DataTable dt = new DataTable();
+                dt.Load(command.ExecuteReader());
+                dgPieces.ItemsSource = dt.DefaultView;
+            }
+            catch (MySqlException erreur)
+            {
+                MessageBox.Show("Erreur de requête SQL :\n" + erreur);
+            }
+            finally
+            {
+                maConnexion.Close();
+            }
         }
 
         private void bNvPiece_Click(object sender, RoutedEventArgs e)
         {
-            NouvellePiece windowNvPiece = new NouvellePiece(maConnexion);
+            NouvellePiece windowNvPiece = new NouvellePiece(maConnexion, this);
             windowNvPiece.Show();
         }
+
+        private void bSupprimer_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgPieces.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Sélectionnez une pièce");
+            }
+            else
+            {
+                string idPiece = "";
+
+                try
+                {
+                    maConnexion.Open();
+
+                    foreach (DataRowView ligne in dgPieces.SelectedItems)
+                    {
+                        idPiece = ligne.Row.Field<string>(0); // récupération de l'id pièce à suppr
+
+                        MySqlParameter id_piece = new MySqlParameter("@id_piece", MySqlDbType.VarChar);
+                        id_piece.Value = idPiece;
+
+                        MySqlCommand command = maConnexion.CreateCommand();
+                        command.CommandText = "DELETE FROM piece WHERE id_piece = @id_piece;";
+                        command.Parameters.Add(id_piece);
+
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("La pièce " + idPiece + " a bien été supprimée");
+                    }
+                }
+                catch (MySqlException erreur)
+                {
+                    MessageBox.Show("Erreur à la suppression de la pièce " + idPiece + " :\n" + erreur);
+                }
+                finally
+                {
+                    maConnexion.Close();
+                    this.LoadInfosPieces(); // actualisation de la liste des pièces
+                }
+            }
+
+        }
+
     }
 }
